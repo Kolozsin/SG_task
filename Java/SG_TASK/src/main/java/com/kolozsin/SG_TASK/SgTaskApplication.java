a308a375c4fd402bf97a493fad9e4195c857e475
unknown <konolearning@gmail.com>
2023-08-15 22:31:35 +0200

    SG_task#2 Created Spring application

---
+++ Java/SG_TASK/src/main/java/com/kolozsin/SG_TASK/SgTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kolozsin.SG_TASK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgTaskApplication.class, args);
+	}
+
+}
